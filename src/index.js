26291d70b0f932d0e9f0d9f44f93b6689e849e32
matcha <35285106+msp5382@users.noreply.github.com>
2019-01-20 18:59:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,6 +34,12 @@ low(adapter).then(db => {
 
   //support parsing of application/x-www-form-urlencoded post data
   app.use(bodyParser.urlencoded({ extended: true }));
+  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
   // our server instance
   const server = http.createServer(app);
